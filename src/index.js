c8fb5925e686f2d4d3a8279aafefa24c5472b3a4
MsMirzakhanova <98583249+MsMirzakhanova@users.noreply.github.com>
2022-11-16 14:22:32 +0200

    process

--- src/index.js
+++ src/index.js
@@ -8,9 +8,6 @@ import SimpleLightbox from "simplelightbox";
 // Дополнительный импорт стилей
 import "simplelightbox/dist/simple-lightbox.min.css";
 
-
-
-
 const searchForm = document.querySelector(`.search-form`)
 const gallery = document.querySelector(`.gallery`)
 const loadMoreBtn = document.querySelector(`.load-more`)
